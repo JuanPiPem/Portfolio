bb963d6db0c335faf0d54d381c66ac2c098dbe50
JuanPiPem <jplezcano75@gmail.com>
2024-06-20 14:17:38 -0700

    add images to carousel

---
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const ImageCarousel = ({ images }) => {
+  return (
+    <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true}>
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Slide ${index}`} />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImageCarousel;
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-
+// src/components/Projects/ProjectCard.js
+import React, { useState } from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
+import ImageCarousel from "../ImageCarousel/ImageCarousel";
 
-export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, visit, source },
-  onDemoClick,
-}) => {
+export const ProjectCard = ({ project }) => {
+  const { title, imageSrc, description, skills, demo, visit, source } = project;
+  const [showCarousel, setShowCarousel] = useState(false);
+
+  const handleDemoClick = (e) => {
+    e.preventDefault();
+    if (title === "Box Delivery") {
+      setShowCarousel(true);
+    } else {
+      window.open(demo, "_blank");
+    }
+  };
+
+  const boxDeliveryImages = [
+    getImageUrl("projects/boxCarousel/boxlogin.png"),
+    getImageUrl("projects/boxCarousel/createacount.png"),
+    getImageUrl("projects/boxCarousel/declaracionjurada.png"),
+    getImageUrl("projects/boxCarousel/gestionarpedidos.png"),
+    getImageUrl("projects/boxCarousel/repartoencurso.png"),
+    getImageUrl("projects/boxCarousel/repartidores.png"),
+    getImageUrl("projects/boxCarousel/agregarpaquetes.png"),
+    getImageUrl("projects/boxCarousel/paquetes.png"),
+    getImageUrl("projects/boxCarousel/iniciarjornada.png"),
+  ];
+
   return (
     <div className={styles.container}>
       <img
@@ -19,17 +41,15 @@ export const ProjectCard = ({
         <p className={styles.description}>{description}</p>
       </div>
       <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
+        {skills.map((skill, id) => (
+          <li key={id} className={styles.skill}>
+            {skill}
+          </li>
+        ))}
       </ul>
       <div className={styles.links}>
         {demo && (
-          <a href={demo} className={styles.link}>
+          <a href={demo} className={styles.link} onClick={handleDemoClick}>
             Demo
           </a>
         )}
@@ -42,6 +62,9 @@ export const ProjectCard = ({
           Source
         </a>
       </div>
+      {showCarousel && title === "Box Delivery" && (
+        <ImageCarousel images={boxDeliveryImages} />
+      )}
     </div>
   );
 };
