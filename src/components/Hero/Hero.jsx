0a97b530c446069ced9dee519c5339115d0f0b8f
JuanPiPem <jplezcano75@gmail.com>
2024-06-25 00:07:40 -0700

    add cv to assets

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,9 +12,44 @@ export const Hero = () => {
           I'm a full-stack developer with 2 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:jplezcano75@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.action}>
+          <a
+            href="mailto:jplezcano75@gmail.com"
+            className={styles.contactBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact Me
+          </a>
+          <a
+            href="/Users/jplezcano/Portfolio/react-portfolio/assets/Juan-Pablo-Lezcano-Resume-20240604 (1).pdf"
+            className={styles.contactBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
+                clip-rule="evenodd"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            CV
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/hero2.png")}
